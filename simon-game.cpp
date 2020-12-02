f323718623072296d041913981ea5bf2ef23f98b
ryanlararr <ryan.arreola@hotmail.com>
2020-12-02 17:25:48 -0500

    Initial commit - Setup Nana
    Nana libraries setup. Fully functional and compiles successfully.

---
+++ simon-game.cpp
@@ -0,0 +1,12 @@
+﻿#include "simon-game.h"
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
